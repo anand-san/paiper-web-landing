aa7c19f530c2722288c3285570c9e9a9aa59963f
Anand Sandilya <17026547+anand-san@users.noreply.github.com>
2024-10-13 12:35:04 +0200

    feat: update bg particles

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE =
-  "Introducing paiper.app: The Future of Document Management is Here";
+export const SITE_TITLE = "Paiper: The Future of Document Management is Here";
 export const SITE_DESCRIPTION =
   "Discover paiper.app, the AI-powered document assistant that's revolutionizing how we manage receipts, invoices, and important papers. Join the waitlist for early access!";
 export const SITE_URL = "https://paiper.app";
