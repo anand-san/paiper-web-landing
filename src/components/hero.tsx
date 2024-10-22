85caa54d4c6e521c7dac17397506392c239ccbb0
Anand Sandilya <anand.sandilya@quantilope.com>
2024-10-22 16:36:35 +0200

    fix: early access form

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,10 +1,43 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import BlurFade from "./ui/blur-fade";
 import { Button } from "./ui/button";
 import Particles from "./ui/particles";
 import { Input } from "./ui/input";
+import { ReloadIcon } from "@radix-ui/react-icons";
 
 export default function HeroSection() {
+  const [email, setEmail] = useState("");
+  const [successMessage, setSuccessMessage] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(false);
+  const [isSubscribing, setIsSubscribing] = useState(false);
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setErrorMessage(false);
+    setIsSubscribing(true);
+
+    if (email) {
+      try {
+        const response = await fetch(
+          `https://addearlyaccessuser-v4mpmgatrq-uc.a.run.app?email=${encodeURIComponent(
+            email
+          )}`
+        );
+        if (response.status === 200) {
+          setSuccessMessage(true);
+        } else {
+          setErrorMessage(true);
+        }
+      } catch (error) {
+        console.error("Error:", error);
+        setErrorMessage(true);
+      } finally {
+        setIsSubscribing(false);
+      }
+    }
+  };
+
   return (
     <section className="flex justify-center h-[70vh] items-center">
       <div className="text-center max-w-sm md:max-w-lg lg:max-w-xl xl:max-w-3xl">
@@ -32,38 +65,45 @@ export default function HeroSection() {
           </h3>
         </BlurFade>
         <BlurFade delay={0.2 * 5} inView>
-          <form
-            id="emailForm"
-            className="flex gap-1 flex-col md:flex-row justify-center align-middle mx-8 md:mx-10"
-          >
-            <Input
-              id="emailInput"
-              type="email"
-              placeholder="Your email address"
-              className="px-4 py-2 border border-gray-200 rounded-xl focus:outline-none md:max-w-xs"
-              required
-            />
-            <Button
-              id="subscribeButton"
-              type="submit"
-              className="border border-gray-200 rounded-xl"
+          {!successMessage && (
+            <form
+              id="emailForm"
+              className="flex gap-1 flex-col md:flex-row justify-center align-middle mx-8 md:mx-10"
+              onSubmit={handleSubmit}
             >
-              Subscribe
-            </Button>
-          </form>
-          <div
-            id="successMessage"
-            className="hidden text-green-500 text-sm md:text-md lg:text-lg xl:text-xl mt-8 mx-3 md:mx-4"
-          >
-            You are all set, We will reach out to you when the early access
-            opens!
-          </div>
-          <div
-            id="errorMessage"
-            className="hidden text-sm md:text-md lg:text-lg xl:text-xl mt-4 text-red-400 mx-3 md:mx-4"
-          >
-            Could not add that email address. Please try again.
-          </div>
+              <Input
+                id="emailInput"
+                type="email"
+                placeholder="Your email address"
+                className="px-4 py-2 border border-gray-200 rounded-xl focus:outline-none md:max-w-xs"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+              <Button
+                id="subscribeButton"
+                type="submit"
+                className="border border-gray-200 rounded-xl"
+                disabled={isSubscribing}
+              >
+                {isSubscribing && (
+                  <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+                )}
+
+                {isSubscribing ? "Subscribing" : "Subscribe"}
+              </Button>
+            </form>
+          )}
+          {successMessage && (
+            <p className="text-xs md:text-sm lg:text-lg xl:text-xl text-green-400">
+              Thank you for your interest, You are all set!
+            </p>
+          )}
+          {errorMessage && (
+            <p className="text-xs md:text-sm lg:text-lg xl:text-xl text-red-400">
+              Could not add that email address. Please try again.
+            </p>
+          )}
         </BlurFade>
       </div>
       <Particles
