db51a637692c0be6b37a133f1aab262c4ed81180
Anand Sandilya <anand.sandilya@quantilope.com>
2024-10-22 16:43:10 +0200

    feat: trigger fireworks on subscription

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -5,12 +5,42 @@ import { Button } from "./ui/button";
 import Particles from "./ui/particles";
 import { Input } from "./ui/input";
 import { ReloadIcon } from "@radix-ui/react-icons";
+import confetti from "canvas-confetti";
 
 export default function HeroSection() {
   const [email, setEmail] = useState("");
   const [successMessage, setSuccessMessage] = useState(false);
   const [errorMessage, setErrorMessage] = useState(false);
   const [isSubscribing, setIsSubscribing] = useState(false);
+
+  const triggerFireworks = () => {
+    const duration = 5 * 1000;
+    const animationEnd = Date.now() + duration;
+    const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+    const randomInRange = (min: number, max: number) =>
+      Math.random() * (max - min) + min;
+
+    const interval = window.setInterval(() => {
+      const timeLeft = animationEnd - Date.now();
+
+      if (timeLeft <= 0) {
+        return clearInterval(interval);
+      }
+
+      const particleCount = 50 * (timeLeft / duration);
+      confetti({
+        ...defaults,
+        particleCount,
+        origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+      });
+      confetti({
+        ...defaults,
+        particleCount,
+        origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+      });
+    }, 250);
+  };
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -25,6 +55,7 @@ export default function HeroSection() {
           )}`
         );
         if (response.status === 200) {
+          triggerFireworks();
           setSuccessMessage(true);
         } else {
           setErrorMessage(true);
